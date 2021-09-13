75a9938c479b0430507deca4a22d64f65fb82c43
Danilo Vilhena <danilo.vilhena@edu.unifor.br>
2021-09-12 23:53:53 -0300

    Header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Logo from '../assets/logo.svg'
+import "./Header.scss"
+import "./Header.responsivity.scss"
+
+const Header = () => {
+    const startHour = 11, endHour = 20
+    const startTime = startHour * 60, endTime = endHour * 60
+    const now = new Date().getHours() * 60 + new Date().getMinutes();
+    const isOpen = startTime <= now && now <= endTime
+
+    return (
+        <header>
+            <div>
+                <img src={Logo} alt="Logomarca da Fatburger"></img>
+                <section>
+                    <h2>Fatburger</h2>
+                    <address>Av. Duque de Caxias, 1326 - Marco, Belém</address>
+                    <p>Pedido mínimo <b>R$20,00</b></p>
+                    <hr></hr>
+                    <div>
+                        {isOpen ?
+                        <span className="badge bg-success">Aberto</span> :
+                        <span className="badge bg-danger">Fechado</span>}
+                        <p>{startHour}:00 - {endHour}:00</p>
+                    </div>
+                </section>
+            </div>
+        </header>
+    )
+}
+
+export default Header
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from '../components/Header';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
 
