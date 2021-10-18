5d6b583fad207dc1dae8d53acc3f93f257f15eba
Danilo Vilhena <danilo.vilhena@edu.unifor.br>
2021-10-18 17:13:26 -0300

    Updating header information

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Logo from '../assets/logo.svg'
+import Logo from '../assets/logo.png'
 import "../styles/Header.scss"
 import "../styles/Header.responsivity.scss"
 
 const Header = () => {
-    const startHour = 11, endHour = 20
+    const startHour = 17, endHour = 23
     const startTime = startHour * 60, endTime = endHour * 60
     const now = new Date().getHours() * 60 + new Date().getMinutes();
     const isOpen = startTime <= now && now <= endTime
@@ -12,10 +12,10 @@ const Header = () => {
     return (
         <header>
             <div>
-                <img src={Logo} alt="Logomarca da Fatburger"></img>
+                <img src={Logo} alt="Logomarca da Dog House Gourmet"></img>
                 <section>
-                    <h2>Fatburger</h2>
-                    <address>Av. Duque de Caxias, 1326 - Marco, Belém</address>
+                    <h2>Dog House Gourmet</h2>
+                    <address>Rua WE-2, 502 - Marambaia, Belém</address>
                     <p>Pedido mínimo <b>R$20,00</b></p>
                     <hr></hr>
                     <div>
